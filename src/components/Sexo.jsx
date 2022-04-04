764ad69713f3990cb64fbfda780bd16b2b388314
juanjopv153 <juanjopv153@gmail.com>
2022-04-04 12:25:17 -0500

    Inicializando el proyecto

---
+++ src/components/Sexo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sexo = () => {
+  return (
+    <div>
+        <span dir='ltr'>Nosé XD</span>
+    </div>
+  )
+}
+
+export default Sexo
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sexo from '../components/Sexo';
+
+function App() {
+  return (
+    <div className="App">
+      <Sexo/>
+    </div>
+  );
+}
+
+export default App;
